97501b98c006fb8e706b5ab84129879fd1d19977
TORTUGAPLATEADA <eduardodelaguila10@gmail.com>
2026-03-21 13:29:47 -0500

    me falto este crud uwu

--- backend/src/routes/hospitales.route.js
+++ backend/src/routes/hospitales.route.js
@@ -11,6 +11,7 @@ router.get('/', async (req, res) => {
   }
 })
 
+//Crear ambulancias EDUARDO SAC
 router.post('/', async (req, res) => {
   const { nombre, latitud, longitud, capacidad_ambulancias } = req.body
   try {
@@ -19,6 +20,32 @@ router.post('/', async (req, res) => {
       [nombre, latitud, longitud, capacidad_ambulancias || 3]
     )
     res.json(resultado.rows[0])
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+//Editar ambulancias EDUARDO SAC
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { nombre, capacidad_ambulancias } = req.body
+  try {
+    const resultado = await pool.query(
+      'UPDATE hospitales SET nombre = $1, capacidad_ambulancias = $2 WHERE id = $3 RETURNING *',
+      [nombre, capacidad_ambulancias, id]
+    )
+    res.json(resultado.rows[0])
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+//Adios vaquero
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    await pool.query('DELETE FROM hospitales WHERE id = $1', [id])
+    res.json({ mensaje: 'Hospital eliminado' })
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
