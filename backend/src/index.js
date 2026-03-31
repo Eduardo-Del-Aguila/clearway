ae24fd94f0d87f3a9a3f9f1b65405f3af74330f2
TORTUGAPLATEADA <eduardodelaguila10@gmail.com>
2026-03-31 01:44:37 -0500

    fix: src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const http = require('http')
 const { Server } = require('socket.io')
-require('dotenv').config()
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 
 const ambulanciasRouter = require('./routes/ambulancias.route')
@@ -18,7 +19,7 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: 
   {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 })
